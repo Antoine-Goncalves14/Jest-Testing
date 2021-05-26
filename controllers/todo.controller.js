5d1c4a24d607048d4135925a365a69d79908173d
Gonçalves Antoine <antoine.goncalves-isc.france@ibm.com>
2021-05-26 16:32:29 +0200

    Refact code and add last testing code

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,8 +1,9 @@
-const todoModel = require('../model/todo.model');
+const TodoModel = require('../model/todo.model');
 
 exports.createTodo = async (req, res, next) => {
   try {
-    const createdModel = await todoModel.create(req.body);
+    const createdModel = await TodoModel.create(req.body);
+
     res.status(201).json(createdModel);
   } catch (e) {
     next(e);
@@ -11,8 +12,54 @@ exports.createTodo = async (req, res, next) => {
 
 exports.getTodos = async (req, res, next) => {
   try {    
-    const allTodos = await todoModel.find({});
+    const allTodos = await TodoModel.find({});
+
     res.status(200).json(allTodos);
+  } catch (e) {
+    next(e);
+  }
+};
+
+exports.getTodoById = async (req, res, next) => {
+  try {
+    const todoModel = await TodoModel.findById(req.params.todoId);
+
+    if (todoModel) {
+      res.status(200).json(todoModel);
+    } else {
+      res.status(404).send();
+    }
+  } catch (e) {
+    next(e);
+  }
+};
+
+exports.updateTodo = async (req, res, next) => {
+  try {
+    const updatedTodo = await TodoModel.findByIdAndUpdate(req.params.todoId, req.body, {
+      new: true,
+      useFindAndModify: false
+    });
+
+    if (updatedTodo) {
+      res.status(200).json(updatedTodo);
+    } else {
+      res.status(404).send();
+    }
+  } catch (e) {
+    next(e);
+  }
+};
+
+exports.deleteTodo = async (req, res, next) => {
+  try {
+    const deletedTodo = await TodoModel.findByIdAndDelete(req.params.todoId);
+
+    if (deletedTodo) {
+      res.status(200).json(deletedTodo);
+    } else {
+      res.status(404).send();
+    }
   } catch (e) {
     next(e);
   }
